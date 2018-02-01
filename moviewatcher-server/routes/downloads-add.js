36aea1aae09a286f7838d29c73fa243dfe0ea9f7
Steven Hermans <steven166@live.nl>
2018-02-01 18:25:03 +0100

    fixed something I dont remember

--- moviewatcher-server/routes/downloads-add.js
+++ moviewatcher-server/routes/downloads-add.js
@@ -6,11 +6,11 @@ var error = require('./route.helper').error;
 var router = express.Router();
 
 router.post('/downloads', (req, res, next) => {
-  if(validate(req.body, res, 'link', 'imdb', 'title')) {
+  if(validate(req.body, res, 'link', 'imdb', 'title', 'path')) {
     let hash = /btih:(.*?)\&/.exec(req.body['link'])[1];
     let download = {hash: hash, title: req.body['title'], imdb: req.body['imdb']};
 
-    config.getDownloadClient().call('add-url', {s: req.body['link']}, (err, downloadList) => {
+    config.getDownloadClient().call('add-url', {s: req.body['link'], path: req.body['path']}, (err) => {
       if (err) {
         error(res, err);
         return;
